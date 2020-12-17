6c7e7dad924fdf283cc410d7512021f322666f7c
Misha <abachyanm@gmail.com>
2020-12-17 18:02:09 +0400

    Full menu page added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+/*Plugins*/
+
 $("#header-slider").owlCarousel({
     items: 1,
     dots: true,
@@ -10,12 +12,19 @@ $('.menu-slider').owlCarousel({
     dotsData: true
 });
 
+$('.menu-categories_list').mCustomScrollbar({
+    axis: "x",
+    mouseWheel: {enable: true},
+    theme: "light"
+});
+
+/*End of Plugins*/
 
 
 $('.menu-categories_list ul li a').on('click', function (e) {
     e.preventDefault();
     $('.menu-slider').hide();
-   $("#"+$(this).attr('href')).show();
+    $("#" + $(this).attr('href')).show();
 });
 
 $('.menu_reservation-list').mCustomScrollbar({
@@ -24,47 +33,25 @@ $('.menu_reservation-list').mCustomScrollbar({
     theme: "light"
 });
 
-$('.menu-categories_list').mCustomScrollbar({
-    axis: "x",
-    mouseWheel: {enable: true},
-    theme: "light"
-});
-
 $('.menu-categories_list ul li').on('click', function () {
-    $('.menu-categories_list ul li').removeClass('active-category')
+    $('.menu-categories_list ul li').removeClass('active-category');
     $(this).addClass('active-category')
-})
-
-let sliderMain = $('.top-header');
-
-
-sliderMain.on('mouseover', function () {
-    $('.item').addClass('slider-main-open');
-    $('.social').addClass('social-hovered');
-    $('.reservation-text').addClass('reservation-text-hovered');
-    $('.header-info-section').addClass('header-info-section-hovered');
-    $('.top-header').addClass('top-header-hovered');
-    $('.top-header_right').addClass('top-header_right_hovered');
-    $('.address').addClass('address-open');
-    $('.tel-number').addClass('tel-number-open');
-    $('#header-slider').addClass('header-slider-hovered')
-    $('.logo_light').css({'opacity': 0});
-    $('.logo_dark').css({'opacity': 1});
 });
 
-sliderMain.on('mouseleave', function () {
-    $('.item').removeClass('slider-main-open');
-    $('.social').removeClass('social-hovered');
-    $('.reservation-text').removeClass('reservation-text-hovered');
-    $('.header-info-section').removeClass('header-info-section-hovered');
-    $('.address').removeClass('address-open');
-    $('.tel-number').removeClass('tel-number-open');
-    $('.top-header').removeClass('top-header-hovered');
-    $('#header-slider').removeClass('header-slider-hovered')
-    $('.top-header_right').removeClass('top-header_right_hovered');
-    $('.logo_light').css({'opacity': 1});
-    $('.logo_dark').css({'opacity': 0});
+
+/*Header Border Animation*/
+
+let topHeader = $('.top-header-home');
+
+topHeader.on('mouseover', function () {
+    $('header').addClass('hovered');
 });
+
+topHeader.on('mouseleave', function () {
+    $('header').removeClass('hovered');
+});
+
+/*End of Header Border Animation*/
 
 
 $('.first-button').on('click', function () {
@@ -76,32 +63,36 @@ $('.menu_reservation-list ul li').on('click', function () {
     $(this).addClass('active')
 });
 
-$('.gallery-img').on('mouseover', function(){
+let galleryImage = $('.gallery-img');
+
+galleryImage.on('mouseover', function () {
     let data = $(this).attr('data');
     $(this).find('.img-text span').text(data);
-    $(this).find('.img-text span').addClass('img-text-bordered ')
+    $(this).find('.img-text span').addClass('img-text-bordered ');
     $(this).find('img').css({'opacity': 0.6})
 });
 
-$('.gallery-img').on('mouseleave', function () {
+galleryImage.on('mouseleave', function () {
     $(this).find('img').css({'opacity': 1})
-})
+});
 
 
-$('.gallery-img').on('mouseleave', function(){
+galleryImage.on('mouseleave', function () {
     $(this).find('.img-text span').text('');
     $(this).find('.img-text span').removeClass('img-text-bordered ')
 });
 
-$('.anons-img').on('mouseover', function (){
-    $(this).find('.border-div').addClass('border-div-bordered')
-    $(this).find('.overlay').addClass('overlay-show')
-})
+let anonsImage = $('.anons-img');
 
-$('.anons-img').on('mouseleave', function (){
-    $(this).find('.border-div').removeClass('border-div-bordered')
+anonsImage.on('mouseover', function () {
+    $(this).find('.border-div').addClass('border-div-bordered');
+    $(this).find('.overlay').addClass('overlay-show')
+});
+
+anonsImage.on('mouseleave', function () {
+    $(this).find('.border-div').removeClass('border-div-bordered');
     $(this).find('.overlay').removeClass('overlay-show')
-})
+});
 
 
 $.fn.datepicker.languages['ru-RU'] = {
@@ -119,14 +110,14 @@ $.fn.datepicker.languages['ru-RU'] = {
 
 
 $('[data-toggle="datepicker"]').datepicker({
-    autoHide:true,
+    autoHide: true,
     language: 'ru-RU'
 });
 
 
 $('#timepicker').mdtimepicker({
-    timeFormat:'hh:mm:ss.000',
-    theme:'dark',
+    timeFormat: 'hh:mm:ss.000',
+    theme: 'dark',
 });
 
 $('.reserv-btn button').on('click', function (event) {
@@ -134,8 +125,21 @@ $('.reserv-btn button').on('click', function (event) {
 });
 
 $('.move-to-up').on('click', function () {
-   $('html').animate({scrollTop:0}, 2000);
+    $('html').animate({scrollTop: 0}, 2000);
 });
+
+
+/*Burger menu*/
+
+$('.burger-navigation-btn').on('click', function () {
+    $('.burger-menu').addClass('burger-menu-open')
+    $('html body').addClass('menu-open')
+})
+
+$('.burger-menu-close-btn').on('click', function () {
+    $('.burger-menu').removeClass('burger-menu-open')
+    $('html body').removeClass('menu-open')
+})
 
 
 
